a4fc8bb08b2e05031920982911a4bc25f972e6be
AND1001 <AND1001@7A-32>
2018-11-19 16:52:32 +0900

    2

--- Puyo/src/puyo/Field.java
+++ Puyo/src/puyo/Field.java
@@ -1,13 +1,65 @@
 package puyo;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class Field extends JPanel {
 	private Puyo[][] puyoArray;
-	
+	private Timer downTimer;
+	private Puyo[] kumiPuyo;
+
+	public Field() {
+		setLayout(null);
+	}
+
 	public void paintComponent(Graphics g) {
-		
+	}
+
+	boolean isTherePuyo(int frameX, int frameY) {
+		if (puyoArray[frameX][frameY] == null) {
+			return false;
+		}
+		return true;
+	}
+
+	public void processAllDown() {
+		for (int i = 12; i > 1; i--) {
+			for (int j = 0; j < 6; j++) {
+				if (puyoArray[j][i].getUnderSpace() == 0)
+					return;
+				puyoArray[j][i].downStairs();
+			}
+		}
+	}
+
+	private void startNewPuyo() {
+		downTimer = new Timer(500, new PuyoListener());
+	}
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(310,610);
+	}
+
+	class PuyoListener implements ActionListener {
+		public void actionPerformed(ActionEvent e) {
+			if (e.getSource() == downTimer) {
+				boolean puyo0 = kumiPuyo[0].naturalDrop();
+				boolean puyo1 = kumiPuyo[1].naturalDrop();
+
+				if (puyo0 == false) {
+					puyoArray[kumiPuyo[0].getFrameX()][kumiPuyo[0].getFrameY()] = kumiPuyo[0];
+				}
+				if (puyo1 == false) {
+					puyoArray[kumiPuyo[1].getFrameX()][kumiPuyo[1].getFrameY()] = kumiPuyo[1];
+				}
+				if(puyo0 == false && puyo1 == false)downTimer.stop();
+
+			}
+		}
 	}
 }
--- Puyo/src/puyo/Puyo.java
+++ Puyo/src/puyo/Puyo.java
@@ -1,24 +1,35 @@
 package puyo;
 
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
+import javax.swing.SwingUtilities;
 
 public class Puyo extends Component {
 	private Image image;
-	private Container container;
-	private int x, y;
+	private Field container;
+	/**
+	 * If puyo which is under this puyo, it will increment this variable.
+	 */
+	private int underSpace = 0;
+	/**
+	 * Position in the field by dot
+	 */
+	private int posX, posY;
+	/**
+	 * Position in the field by fram(6 x 12)
+	 */
+	private int frameX, frameY;
 	private int colorNumber;
 	private static Image[] puyoImageArray; //redPuyoImage, greenPuyoImage, bluePuyoImage, yellowPuyoImage, yellowGreenPuyoImage, grayPuyoImage;
 
 	/**
 	 * How many puyos are connected including this puyo.
 	 */
-	private int connectNumber;
+	private int connectNumber = 1;
 
 	static {
 		try {
@@ -35,23 +46,79 @@ public class Puyo extends Component {
 		}
 	}
 
-public Puyo(Container container, int color) {
-	this.image = image;
-	this.container = container;
-	this.colorNumber = color;
-}
-public Puyo(int color) {
-	this.colorNumber = color;
-}
+	public Puyo(Field container, int color) {
+		this.image = puyoImageArray[color];
+		this.container = container;
+		this.colorNumber = color;
+	}
 
-void setContainer(Container container) {
-	this.container = container;
-}
+	public Puyo(int color) {
+		this.colorNumber = color;
+	}
 
-public int getColorNumber() {
-	return colorNumber;
-}
-public void downStairs() {
+	public int getUnderSpace() {
+		return underSpace;
+	}
 
-}
+	void increaseUnderSpace() {
+		underSpace++;
+	}
+
+	int getFrameX() {
+		return frameX;
+	}
+	int getFrameY(){
+		return frameY;
+	}
+	void setContainer(Field container) {
+		this.container = container;
+	}
+
+	public int getColorNumber() {
+		return colorNumber;
+	}
+
+	public boolean naturalDrop() {
+		if (container.isTherePuyo(frameX, frameY + 1))
+			return false;
+		SwingUtilities.invokeLater(new PuyoDropper());
+		return true;
+	}
+
+	public void downStairs() {
+		for (int roop = 0; roop < underSpace; roop++) {
+			Boolean isFinish = false;
+			Thread dropper = new PuyoDropper();
+			SwingUtilities.invokeLater(dropper);
+			try {
+				while (dropper.isAlive()) {
+					Thread.sleep(50);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		underSpace = 0;
+	}
+
+	class PuyoDropper extends Thread {
+		private int interY;
+
+		public PuyoDropper() {
+		}
+
+		//repaint(long tm, int x, int y, int width, int height)
+		public void run() {
+			for (int interY = 0; interY < 50; interY += 5) {
+				repaint(50, posX, posY, posX + 50, posY + interY);
+				try {
+					sleep(50);
+				} catch (Exception e) {
+					System.out.println("Puyo Dropper Sleep");
+					e.printStackTrace();
+				}
+			}
+			frameY--;
+		}
+	}
 }
